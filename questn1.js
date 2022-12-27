fc8493ebb6df7fa4d0b9a18f7a041e85e3e25d79
karthik-140 <shanigaramkarthik@gmail.com>
2022-12-27 11:09:37 +0530

    questn2

---
+++ questn1.js
@@ -0,0 +1,8 @@
+
+
+document.title ="Select Items";
+
+console.log(document.all[8])
+document.all[10].textContent='Select Items';
+document.all[10].style.color='purple';
+document.all[10].style.background='white';
